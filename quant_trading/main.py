d23a5ee6b3f408bd34483c7de55781c1aacfdfc2
rdgozum <ryanpaul.gozum@gmail.com>
2020-10-19 02:04:13 +0800

    Set main.py as the entry point module

--- quant_trading/main.py
+++ quant_trading/main.py
@@ -1,1 +1,46 @@
 """Main module."""
+import os
+import sys
+import argparse
+
+sys.path.insert(0, os.path.abspath(".."))
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--start-date",
+        dest="start_date",
+        default="2015-01-01",
+        help="extracts data from the specified start-date (default: 2015-01-01)",
+    )
+    parser.add_argument(
+        "--end-date",
+        dest="end_date",
+        default="2015-12-01",
+        help="extracts data until the specified end-date, (default: 2015-12-01)",
+    )
+    parser.add_argument(
+        "--do-extract-save",
+        dest="do_extract_save",
+        action="store_true",
+        help="extracts and saves the pulled range to csv file",
+    )
+    parser.add_argument(
+        "--do-extract-from-file",
+        dest="do_extract_from_file",
+        action="store_true",
+        help="extracts the pulled range data from csv file",
+    )
+
+    return parser.parse_args()
+
+
+def run(args):
+    pass
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    run(args)
