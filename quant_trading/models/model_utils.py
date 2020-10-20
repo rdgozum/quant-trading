368aa8c184531e38b4194c0d18f276cc0d9280ae
rdgozum <ryanpaul.gozum@gmail.com>
2020-10-20 12:34:30 +0800

    Plot the reconstructed time series

---
+++ quant_trading/models/model_utils.py
@@ -0,0 +1,28 @@
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot(X_test, X_test_pred):
+    n = math.ceil(X_test.shape[0] / X_test.shape[1])
+    plt.figure()
+
+    for i, idx in enumerate(list(np.arange(0, X_test.shape[0], X_test.shape[1]))):
+        # display original
+        ax = plt.subplot(2, n, i + 1)
+        if i == 0:
+            ax.set_ylabel("Input", fontweight=600)
+        else:
+            ax.get_yaxis().set_visible(False)
+        plt.plot(X_test[idx])
+        ax.get_xaxis().set_visible(False)
+
+        # display reconstruction
+        ax = plt.subplot(2, n, i + 1 + n)
+        if i == 0:
+            ax.set_ylabel("Output", fontweight=600)
+        else:
+            ax.get_yaxis().set_visible(False)
+        plt.plot(X_test_pred[idx])
+        ax.get_xaxis().set_visible(False)
+    plt.show()
